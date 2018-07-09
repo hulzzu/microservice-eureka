de5c527f3580cda6a19c9d7a66d2cd074bec5e5d
hqlzzu <1157735700@qq.com>
2018-07-09 15:56:34 +0800

    分支测试0001

--- src/main/java/com/itmch/cloud/EurekaApplication.java
+++ src/main/java/com/itmch/cloud/EurekaApplication.java
@@ -10,5 +10,6 @@ public class EurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 		System.out.println("1111111111111111");
+		System.out.println("1111111111111111");
 	}
 }
